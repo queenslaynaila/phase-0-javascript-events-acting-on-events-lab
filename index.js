47f0c1e4111fbc042a0cb24fc8312a66fd1f1410
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-05 02:21:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+const moveDodgerLeft = () => {
+  const dodger = document.getElementById("dodger");
+  dodger.style.left = "50px";
+};
+// moveDodgerLeft();
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  dodger.style.left = "250px";
+}
