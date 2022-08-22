ea914d0a46e7c4e5a58364058c51504c497ac74f
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-22 11:36:25 +0300

    ASsignment complete

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const moveDodgerLeft = () => {
   const dodger = document.getElementById("dodger");
   dodger.style.left = "50px";
 };
+moveDodgerLeft();
 // moveDodgerLeft();
 function moveDodgerRight() {
   const dodger = document.getElementById("dodger");
   dodger.style.left = "250px";
 }
+moveDodgerRight()
